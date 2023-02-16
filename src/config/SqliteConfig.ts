fce7607888544abd76ce7f67a3166c1f68aba9d2
김호수 <ghtnfhf94@gmail.com>
2023-02-17 00:53:26 +0900

    sqlite 설치 및 적용

---
+++ src/config/SqliteConfig.ts
@@ -0,0 +1,27 @@
+import * as SQLite from "expo-sqlite";
+import { Platform } from "react-native";
+
+const DATABASE_NAME: string = "db.testDb"; // db.데이터베이스 명
+let db: SQLite.WebSQLDatabase | null = null;
+
+/**
+ * 최초 데이터 베이스에 연결한다.
+ * ㄴ 데이터베이스 연결을 위해서는 필수.
+ * @returns {SQLite.WebSQLDatabase} db
+ */
+export const initDatabaseConfig = (): any => {
+  // 웹 플랫폼은 지원하지 않음.
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+  db = SQLite.openDatabase(DATABASE_NAME);
+  // [필수] 코드가 트랜잭션내에서 작동하지 않는 경우
+  db.exec([{ sql: "PRAGMA foreign_keys = ON;", args: [] }], false, () => {});
+  return db;
+};
--- src/screen/home/home.tsx
+++ src/screen/home/home.tsx
@@ -6,17 +6,54 @@ import {
   Text,
   ToastAndroid,
   View,
+  Platform,
 } from "react-native";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import IconButton from "../../component/button/IconButton";
 import CircleButton from "../../component/button/CircleButton";
 import { ScreenName } from "../../constant/Screen";
 import TextItem from "../../component/item/TextItem";
+import { initDatabaseConfig } from "../../config/SqliteConfig";
+
+const TABLE_USER = "tb_user";
+const DBInstance = initDatabaseConfig();
+
+const createUserInfo = async (): Promise<void> => {
+  const query: string = `create table if not exists ${TABLE_USER} 
+          (
+            userSq INTEGER PRIMARY KEY AUTOINCREMENT, 
+            userId TEXT, 
+            userName TEXT
+          )`;
+  return await new Promise((resolve, reject) => {
+    DBInstance.transaction(
+      (tx: any) => {
+        tx.executeSql(query);
+      },
+      () => {
+        reject("Fail to create a todo table");
+      },
+      () => {
+        resolve();
+      }
+    );
+  });
+};
 
 function Home({ navigation }: any) {
   const [todos, setTodos] = useState<TodoTypes[] | null>([
     { index: 1, content: "TEST" },
   ]);
+
+  useEffect(() => {
+    (async () => {
+      _initConnectSqlite(); // [API] 테이블 존재 체크 이후 테이블 생성
+    })();
+  }, []);
+
+  const _initConnectSqlite = useCallback(async () => {
+    await createUserInfo();
+  }, []);
 
   const onAddSticker = () => {
     // we will implement this later
